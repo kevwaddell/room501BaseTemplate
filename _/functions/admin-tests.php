e4cc8288a89fa613af33de0be6d4b47b8ff42992
Kevin Waddell <kevin@room501.co.uk>
2012-05-01 13:09:33 +0100

    Change admin tests file

--- _/functions/admin-tests.php
+++ _/functions/admin-tests.php
@@ -7,42 +7,7 @@ global $posts;
 global $wp_taxonomies;
 global $pagenow, $typenow;
 
-$expire = get_post_meta($post->ID, 'expiration_date', true);
-$end_time = get_post_meta($post_id, 'end_time', true);
-
-$convert_expire = strtotime($expire. " " .$end_time);
-$expire_formated = date('Y-m-d H:i:s', $convert_expire);
-
-/* echo '<pre>';print_r( $expire_formated );echo '</pre>'; */
-
-
-/* echo '<pre>';print_r( get_post_status_object('passed') );echo '</pre>'; */
-
-/*
-$date_format = get_option('date_format');
-$time_format = get_option('time_format');
-$time_zone = get_option('timezone_string');
-date_default_timezone_set($time_zone);
-$now = date('Y-m-d H:i:s', time());
-$today = get_gmt_from_date( $now );
-*/
-
-/*
-echo '<pre>';print_r($typenow);echo '</pre>';
-echo '<pre>';print_r($pagenow);echo '</pre>';
-*/
-
-/*
-	if ($post) {
-	$expire = get_post_meta($post->ID, 'expiration_date', true);
-	$end_time = get_post_meta($post->ID, 'end_time', true);
-	$convert_expire = strtotime($expire. " " .$end_time);
-	$expire_formated = date('Y-m-d H:i:s', $convert_expire);
-
-	echo '<pre>';print_r($expire_formated);echo '</pre>';
-
-	}
-*/
+echo '<pre>';print_r($current_screen);echo '</pre>';
 }
 add_action('admin_head', 'get_options_tests');
 ?>
